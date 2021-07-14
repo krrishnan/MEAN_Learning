2597c88812a003165ebce82738b40c57dfb57cd9
krrishnan <gopikrishnan_s@solartis.com>
2021-07-14 21:39:15 +0530

    Module not found Error fix

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require("D:\\GopiKrishnan\\Projects\\MEAN\\loc8r\\app_server\\controllers\\main.js");
+var ctrlMain = require("../controllers/main");
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
